26dc0ad143364841f4b8d7fc969a1fa85f13b6a6
alberto-forjan-tu-as <berto.forjan@gmail.com>
2026-09-12 00:22:06 +0200

    120920260021-portatil

---
+++ app/src/main/java/com/example/appplaceholder_v20/core/datasource/CommentsDataSorce.kt
@@ -0,0 +1,9 @@
+package com.example.appplaceholder_v20.core.datasource
+
+import com.example.appplaceholder_v20.core.model.DatosCommentsDto
+import javax.inject.Inject
+
+class CommentsDataSorce @Inject constructor() {
+
+
+}
---
+++ app/src/main/java/com/example/appplaceholder_v20/core/model/DatosCommentsDto.kt
@@ -0,0 +1,5 @@
+package com.example.appplaceholder_v20.core.model
+
+data class DatosCommentsDto(
+
+)
